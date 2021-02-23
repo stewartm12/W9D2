7beea1c8762058c3e696b984038a63f5cce5454b
stewart morales <stewartmorales129@gmail.com>
2021-02-23 13:32:37 -0500

    on phase 1

---
+++ src/moving_object.js
@@ -0,0 +1,12 @@
+function MovingObject(pos, vel, radius, color) { 
+  this.pos = pos;
+  this.vel = vel;
+  this.radius = radius;
+  this.color = color;
+}
+
+MovingObect.prototype.draw = function(ctx) {
+  const canvas = 
+}
+
+module.exports = MovingObject;
